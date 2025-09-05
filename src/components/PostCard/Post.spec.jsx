45168c920ed3d72134b3410442ecc0b982abec5c
Raul <raul_duval@hotmail.com>
2025-09-05 15:24:15 -0300

    configurações supremas

---
+++ src/components/PostCard/Post.spec.jsx
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react"
+import { PostCard } from "."
+import { postCardPropsMock } from "./mock"
+
+const props = postCardPropsMock;
+
+describe('<Postcard />', () => {
+    it('should render Postcard correctly', () => {
+        render(<PostCard {...props} />)
+
+        expect(screen.getByRole('img', { name: 'title 1' })).toBeInTheDocument();
+        expect(screen.getByRole('img', { name: /title 1/i }))
+        .toHaveAttribute('src', props.cover);
+        expect(screen.getByRole('heading', { name: '1 title 1' })).toBeInTheDocument();
+        expect(screen.getByText('body 1')).toBeInTheDocument();
+    })
+
+    it('should match snapshot', () => {
+        const { container } = render(<PostCard {...props} />);
+
+        expect(container.firstChild).toMatchSnapshot();
+    });
+})
---
+++ src/templates/Home.spec.jsx
@@ -0,0 +1,5 @@
+describe('Home />', () => {
+  it('is is a dummy test', () => {
+    expect(1).toBe(1);
+  });
+})
